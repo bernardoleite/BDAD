ac8f9a0d5eb4ceec9717c2663d8df72eb79d43fd
r-ferreira <rii.ferreira5@gmail.com>
2017-05-19 11:12:24 +0100

    update

--- 03/trabalho.sql
+++ 03/trabalho.sql
@@ -8,18 +8,18 @@ BEGIN TRANSACTION;
 
 -- Table: Consulta
 CREATE TABLE Consulta (ConsultaIdent INTEGER PRIMARY KEY, Dia DATE, HoraInicio TIME, HoraFim TIME CHECK (HoraInicio < HoraFim), Paciente REFERENCES Paciente (NIF), Medico REFERENCES Medico (NIF), Enfermeiro REFERENCES Enfermeiro (NIF) UNIQUE, UNIQUE (Dia, Paciente, Medico));
-INSERT INTO Consulta VALUES (1, '2017-05-16', 12, 13, 258908, 258909, NULL);
-INSERT INTO Consulta VALUES (2, '2017-05-16', 12, 12.30, 258907, 258910, NULL);
-INSERT INTO Consulta VALUES (3, '2017-05-18', 12.30, 13.30, 258907, 258913, NULL);
-INSERT INTO Consulta VALUES (4, '2017-05-19', 12, 12.30, 258908, 258912, NULL);
-INSERT INTO Consulta VALUES (5, '2017-05-19', 13, 14.30, 258906, 258913, NULL);
+INSERT INTO Consulta VALUES (1, '2017-05-19', 12, 13, 258908, 258909, NULL);
+INSERT INTO Consulta VALUES (2, '2017-05-19', 12, 12.30, 258907, 258910, NULL);
+INSERT INTO Consulta VALUES (3, '2017-05-20', 12.30, 13.30, 258907, 258913, NULL);
+INSERT INTO Consulta VALUES (4, '2017-05-21', 12, 12.30, 258908, 258912, NULL);
+INSERT INTO Consulta VALUES (5, '2017-05-21', 13, 14.30, 258906, 258913, NULL);
 
 -- Table: Departamento
 CREATE TABLE Departamento (Designacao TEXT PRIMARY KEY);
 
 INSERT INTO Departamento VALUES ('OFTALMOLOGIA');
 INSERT INTO Departamento VALUES ('CARDIOLOGIA');
-INSERT INTO Departamento VALUES ('AUTORRINOLARINGOLOGIA');
+INSERT INTO Departamento VALUES ('OTORRINOLARINGOLOGIA');
 
 -- Table: DepEnfermeiro
 CREATE TABLE DepEnfermeiro (Enfermeiro REFERENCES Enfermeiro (NIF), Departamento REFERENCES Departamento (Designação), PRIMARY KEY (Enfermeiro, Departamento));
@@ -49,7 +49,10 @@ CREATE TABLE DiaTécnico (DiaTrabalho REFERENCES DiaTrabalho (Dia), Técnico REFER
 CREATE TABLE Enfermeiro (Nome TEXT, Idade INTEGER, DataNascimento DATE, NIF INTEGER PRIMARY KEY, Telemóvel INTEGER, Departamento REFERENCES Departamento (Designação));
 
 -- Table: Especialidde
-CREATE TABLE Especialidde (Designação TEXT PRIMARY KEY, MedicoChefe REFERENCES Medico (NIF));
+CREATE TABLE Especialidde (Sigla TEXT PRIMARY KEY,Designação TEXT , MedicoChefe REFERENCES Medico (NIF));
+
+INSERT INTO Especialidade VALUES('CARD', 'CARDIOLOGIA', 258912);
+INSERT INTO Especialidade VALUES('OFTA', 'OFTALMOLOGIA', 258910);
 
 -- Table: Exame
 CREATE TABLE Exame (ExameIdent INTEGER PRIMARY KEY, Dia INTEGER, HoraInicio TIME, HoraFim TIME CHECK (HoraInicio < HoraFim), Resultado STRING, Consulta REFERENCES Consulta (ConsultaIdent), Exame REFERENCES TipoExame);
@@ -61,13 +64,13 @@ CREATE TABLE Funcionário (Nome TEXT, Idade INTEGER, DataNascimento DATE, NIF INT
 CREATE TABLE Limpeza (Funcionário REFERENCES Funcionário (NIF), SalaExame REFERENCES SalaExame (ID), PRIMARY KEY (Funcionário, SalaExame));
 
 -- Table: Médico
-CREATE TABLE Medico (Nome TEXT, Idade INTEGER, DataNascimento DATE, NIF INTEGER PRIMARY KEY, Telemóvel INTEGER, Especialidade REFERENCES Especialidade (Designacao));
+CREATE TABLE Medico (Nome TEXT, Idade INTEGER, DataNascimento DATE, NIF INTEGER PRIMARY KEY, Telemóvel INTEGER, Especialidade REFERENCES Especialidade (Sigla));
 
-INSERT INTO Medico VALUES ('Orlando Mendes', '40', 1970, 258909, 919028760, 'CARDIOLOGIA');
-INSERT INTO Medico VALUES ('Miguel Vaz', '41', 1960, 258910, 919028799, 'OFTALMOLOGIA');
-INSERT INTO Medico VALUES ('Rita Malaco', '32', 1980, 258911, 919028800, 'OFTALMOLOGIA');
-INSERT INTO Medico VALUES ('Joana Carvalho', '52', 1980, 258912, 919028800, 'CARDIOLOGIA');
-INSERT INTO Medico VALUES ('Bernardo Martins', '52', 1980, 258913, 919028800, 'CARDIOLOGIA');
+INSERT INTO Medico VALUES ('Orlando Mendes', '40', 1970, 258909, 919028760, 'CARD');
+INSERT INTO Medico VALUES ('Miguel Vaz', '41', 1960, 258910, 919028799, 'OFTA');
+INSERT INTO Medico VALUES ('Rita Malaco', '32', 1980, 258911, 919028800, 'OFTA');
+INSERT INTO Medico VALUES ('Joana Carvalho', '52', 1980, 258912, 919028800, 'CARD');
+INSERT INTO Medico VALUES ('Bernardo Martins', '52', 1980, 258913, 919028800, 'CARD');
 
 -- Table: Paciente
 CREATE TABLE Paciente (Nome TEXT, Idade INTEGER, DataNascimento DATE, NIF INTEGER PRIMARY KEY, Telemóvel INTEGER, SeguroIdent REFERENCES Seguro (SeguroIdent));
